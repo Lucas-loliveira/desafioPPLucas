a1a84253db68794f30c100790b5266fd7abfe978
LucasPIB <lucasoliveira783@gmail.com>
2020-10-22 20:06:21 -0300

    integracao entre mapas e rotas

--- setup/desafio/models.py
+++ setup/desafio/models.py
@@ -7,9 +7,12 @@ class Mapa(models.Model):
         return self.nome
 
 class Rota(models.Model):
+    mapa = models.ForeignKey(Mapa, related_name='rotas', on_delete=models.CASCADE)
     origem = models.CharField(max_length = 30)
     destino = models.CharField(max_length = 30)
     distancia = models.CharField(max_length = 30)
-    
+    class Meta:
+        unique_together = ['mapa', 'origem','destino','distancia']
+
     def __str__(self):
-        return self.origem
+        return '%s: %s' % (self.origem, self.destino)
--- setup/desafio/serializer.py
+++ setup/desafio/serializer.py
@@ -1,12 +1,23 @@
 from rest_framework import serializers 
 from desafio.models import Mapa, Rota
 
-class MapaSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Mapa
-        fields = '__all__'
 
 class RotaSerializer(serializers.ModelSerializer):
      class Meta:
         model = Rota
-        fields = '__all__'
+        fields =  ['origem',  'destino', 'distancia']
+
+
+class MapaSerializer(serializers.ModelSerializer):
+    rotas = RotaSerializer(many=True)
+    class Meta:
+        model = Mapa
+        fields = ['nome',  'rotas']
+
+    def create(self, validated_data):
+        rotas_data = validated_data.pop('rotas')
+        mapa = Mapa.objects.create(**validated_data)
+        for rota_data in rotas_data:
+            Rota.objects.create(mapa=mapa, **rota_data)
+        return mapa
+

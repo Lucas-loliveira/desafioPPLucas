0808d97ed0e79f4a9bba5c005cc86da0c0483523
LucasPIB <lucasoliveira783@gmail.com>
2020-10-25 16:08:54 -0300

    att url heruku

--- setup/setup/settings.py
+++ setup/setup/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://desafio-pp-lucas.herokuapp.com/']
+ALLOWED_HOSTS = ['https://desafio-proximap-lucas.herokuapp.com/']
 
 
 # Application definition

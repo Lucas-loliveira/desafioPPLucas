2cb9ce218e7d60bef95de60cfef651082bdb4c4a
LucasPIB <lucasoliveira783@gmail.com>
2020-10-25 11:28:18 -0300

    corrigindo status http, criacao de testes unitarios, correcao checkRequestValido

--- setup/desafio/tests.py
+++ setup/desafio/tests.py
@@ -1,3 +1,82 @@
 from django.test import TestCase
+import pytest
+import json
 
-# Create your tests here.
+
+#constantes
+rotasMapaValid = [{"origem":"A", "destino":"B", "distancia": 10},{"origem":"B", "destino":"D", "distancia": 15},{"origem":"A", "destino":"C", "distancia": 20},{"origem":"C", "destino":"D", "distancia": 30},{"origem":"B", "destino":"E", "distancia": 50},{"origem":"D", "destino":"E", "distancia": 30},{"origem":"Y", "destino":"Z", "distancia": 30}]
+rotasMapaInvalid =  [{"or2igem":"B", "destino":"D", "distancia": 15},{"origem":"B", "destino":"D", "distancia": 15}]
+mapaValid = "mapa teste"
+origemValid = "B"
+destinoValid = "D"
+autonomiaValid = 10
+valorLitroValid = 3
+mapaInvalid = "mapa errado"
+origemTypeInvalid  = 333
+destinoInvalid  = "destino errado"
+autonomiaInvalid  = -10
+valorLitroInvalid  = 0
+
+
+@pytest.fixture
+def api_client():
+   from rest_framework.test import APIClient
+   return APIClient()
+
+@pytest.mark.django_db(transaction=True)
+@pytest.mark.parametrize(
+   'nome, rotas, status_code', [
+       ("", "", 400),
+       ("",rotasMapaValid , 400),
+       (mapaValid,rotasMapaInvalid, 400),
+       (mapaValid,rotasMapaValid, 201),
+   ]
+)
+def test_mapa(
+   nome, rotas, status_code, api_client
+):
+   url = '/mapas/'
+   data = {
+       'nome': nome,
+       'rotas': rotas
+   }
+   response = api_client.post(url, content_type='application/json', data=json.dumps(data))
+   assert response.status_code == status_code
+
+
+
+
+@pytest.mark.django_db(transaction=True)
+@pytest.mark.parametrize(
+   'mapa, origem, destino, autonomia, valorLitro, status_code', [
+       (mapaValid,origemValid ,destinoValid,autonomiaValid,valorLitroValid, 200),#valido
+       (mapaValid,origemTypeInvalid ,destinoValid,autonomiaValid,valorLitroValid, 400),#tipo origem invalido
+       (mapaValid,origemValid ,destinoValid,autonomiaInvalid,valorLitroValid, 400),#autonomia invalida
+       (mapaValid,origemValid ,destinoValid,autonomiaValid,valorLitroInvalid, 400),#valorLitro invalido
+       (mapaInvalid,origemValid ,destinoValid,autonomiaValid,valorLitroValid, 404),#mapa nao encontrado
+       (mapaValid,origemValid ,destinoInvalid,autonomiaValid,valorLitroValid, 404),#destino nao encontrado no mapa selecionado
+   ]
+)
+def test_calculo(
+   mapa, origem, destino, autonomia, valorLitro, status_code, api_client
+):
+    #criando mapa valido para teste de rota
+    url = '/mapas/'
+    data = {
+        'nome': mapaValid,
+        'rotas': rotasMapaValid
+    }
+    response = api_client.post(url, content_type='application/json', data=json.dumps(data))
+
+    #teste de rota
+    url = '/calculoRota/'
+    data = {
+        'mapa': mapa,
+        'origem': origem,
+        'destino': destino,
+        'autonomia': autonomia,
+        'valorLitro': valorLitro
+
+    }
+    response = api_client.post(url, content_type='application/json', data=json.dumps(data))
+    assert response.status_code == status_code
--- setup/desafio/views.py
+++ setup/desafio/views.py
@@ -18,13 +18,14 @@ class RotasViewSet(viewsets.ModelViewSet):
     queryset = Rota.objects.all()
     serializer_class = RotaSerializer
 
-@api_view(['GET'])
+@api_view(['GET','POST'])
 def CalculoRota(request):
     parametros =  JSONParser().parse(request)
 
+    
     if not checkRequestValido(parametros):
-        return JsonResponse({'mensagem': 'Parametros invalidos'}, status=status.HTTP_404_NOT_FOUND)
-
+        return JsonResponse({'mensagem': 'Parametros invalidos'}, status=status.HTTP_400_BAD_REQUEST)
+    
     #obter rotas
     rotas = Rota.objects.all()
     rotas = rotas.filter(mapa__nome__icontains= parametros['mapa'])
@@ -34,10 +35,10 @@ def CalculoRota(request):
         return JsonResponse({'mensagem': 'Mapa nao encontrado'}, status=status.HTTP_404_NOT_FOUND)
 
     if not  rotas.filter(origem__icontains= parametros['origem']):
-        return JsonResponse({'mensagem': 'Origem nao encontrada'}, status=status.HTTP_404_NOT_FOUND)
+        return JsonResponse({'mensagem': 'Origem nao encontrada no mapa'}, status=status.HTTP_404_NOT_FOUND)
     
     if not  rotas.filter(destino__icontains= parametros['destino']):
-        return JsonResponse({'mensagem': 'Destino nao encontrado'}, status=status.HTTP_404_NOT_FOUND)
+        return JsonResponse({'mensagem': 'Destino nao encontrado no mapa'}, status=status.HTTP_404_NOT_FOUND)
 
     
     graph = Graph()
@@ -49,18 +50,23 @@ def CalculoRota(request):
 
     gasto = dijkstra.get_distance(parametros['destino'])/parametros['autonomia']*parametros['valorLitro']
 
-    return JsonResponse({'origem': parametros['origem'],'destino': parametros['destino'],'gasto': gasto,'rota': caminho}, status=status.HTTP_404_NOT_FOUND) 
+    return JsonResponse({'origem': parametros['origem'],'destino': parametros['destino'],'gasto': gasto,'rota': caminho}, status=status.HTTP_200_OK) 
          
 
 def checkRequestValido(parametros):
     #checar parametros string
-    if not isinstance(parametros['mapa'], str) and  isinstance(parametros['origem'], str) and isinstance(parametros['destino'], str) and isinstance(parametros['origem'], str):
+    if not (isinstance(parametros['mapa'], str) and  isinstance(parametros['origem'], str) and isinstance(parametros['destino'], str) and isinstance(parametros['origem'], str)):
         return False
     #checar parametros float
-    if not  isinstance(parametros['autonomia'], float) and  isinstance(parametros['valorLitro'], float):
+    if not  (isinstance(parametros['autonomia'], (int, float)) and  isinstance(parametros['valorLitro'], (int, float))):
         return False
     #checar parametros positivos
-    if not  parametros['autonomia'] > 0 and  parametros['valorLitro'] >= 0 :
+    if not  (parametros['autonomia'] > 0 and  parametros['valorLitro'] > 0) :
         return False
-    
+
     return True
+
+
+
+
+
